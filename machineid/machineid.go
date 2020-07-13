700c05b0980b0a507645e92aa4467ad75e6f3d3c
MeloQi <sto_dongdong@163.com>
2020-07-13 14:16:48 +0800

    machineid 添加docker获取宿主机id

--- machineid/machineid.go
+++ machineid/machineid.go
@@ -1,6 +1,6 @@
 package machineid
 
-import "github.com/denisbrodbeck/machineid"
+import "github.com/MeloQi/machineid"
 
 func GetMachineid(appID string) (string, error) {
 	return machineid.ProtectedID(appID)
